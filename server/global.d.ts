530b1bf101174a1fe5642bcd682126773f2e6a33
TianyiLi <tianyili@TianyiLis-MBP.hub>
2024-04-12 19:26:41 +1000

    update meta files

---
+++ server/global.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    PORT: string;
+    MONGODB_URI: string;
+    OPENAI_API_KEY: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+  }
+}
